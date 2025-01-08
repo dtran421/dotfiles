b27e8ccec7ab8682e351c79a5f3c39d629a2c92f
dtran <duketran2001@gmail.com>
2025-01-07 23:18:16 -0500

    fixed tmux + ghostty config

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,9 +2,12 @@ function ColorScheme(color)
   color = color or "tokyonight"
   vim.cmd.colorscheme(color)
 
-  vim.api.nvim_set_hl(0, "Normal", { bg = "#181826" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#313245" })
-  vim.api.nvim_set_hl(0, "Pmenu", { bg = "#313245" })
+  -- vim.api.nvim_set_hl(0, "Normal", { bg = "#181826" })
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#313245" })
+  -- vim.api.nvim_set_hl(0, "Pmenu", { bg = "#313245" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFLoat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
   vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 
