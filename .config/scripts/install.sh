a92bcb61763000f236a5067864c3e7cc367a4ba3
dtran <duketran2001@gmail.com>
2025-11-17 14:09:27 -0500

    Misc maintenance

--- .config/scripts/install.sh
+++ .config/scripts/install.sh
@@ -1,7 +1,16 @@
 #!/bin/zsh
 
+OS_NAME=$(uname -s)
+
 echo "Installing dotfiles"
 
 ./install-ghostty.sh
+
+echo "Misc setup"
+
+if [[ "$OS_NAME" == "Darwin" ]]; then
+  echo "Installing fonts"
+  curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.47/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+fi
 
 echo "Done!"
